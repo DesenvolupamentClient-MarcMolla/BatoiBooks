2d8aa03d68217a23555d0de74a186f9f38851ae6
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2024-10-07 15:52:06 +0200

    Proyecto correctamente inicializado

---
+++ src/functions.js
@@ -0,0 +1,48 @@
+function getBookById() {}
+
+function getBookIndexById() {}
+
+function bookExists() {}
+
+function booksFromUser() {}
+
+function booksFromModule() {}
+
+function booksCheeperThan() {}
+
+function booksWithStatus() {}
+
+function averagePriceOfBooks() {}
+
+function booksOfTypeNote() {}
+
+function booksNotSold() {}
+
+function incrementPriceOfbooks() {}
+
+function getUserById() {}
+
+function getUserIndexById() {}
+
+function getUserByNickName() {}
+
+function getModuleByCode() {}
+  
+
+export {
+    getBookById,
+    getBookIndexById,
+    bookExists,
+    booksFromUser,
+    booksFromModule,
+    booksCheeperThan,
+    booksWithStatus,
+    averagePriceOfBooks,
+    booksOfTypeNote,
+    booksNotSold,
+    incrementPriceOfbooks,
+    getUserById,
+    getUserIndexById,
+    getUserByNickName,
+    getModuleByCode
+  }
